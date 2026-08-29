c051585f74a4b43b87edce5010191f7fe85ba6e3
Parasar.eth <133246366+pkbtc@users.noreply.github.com>
2026-08-29 17:41:14 +0530

    Add: gas-station - submission-0

---
+++ Data Structures & Algorithms/gas-station/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas=0;
+        int tank=0;
+        int start=0;
+        
+        for(int i=0;i<gas.length;i++){
+            int gain=gas[i]-cost[i];
+            tank+=gain;
+            totalGas+=gain;
+            if(tank<0){
+                start=i+1;
+                tank=0;
+            }
+
+        }
+        if(totalGas<0){
+            return -1;
+        }
+        return start;
+    }
+}
